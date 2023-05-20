f5c77debae04f1e587a006c27d52778189f33cc2
tabrown3 <t-a-brown@hotmail.com>
2023-05-19 20:23:09 -0500

    get large bounds working

--- Assets/SpriteScrollerLarge.cs
+++ Assets/SpriteScrollerLarge.cs
@@ -2,19 +2,20 @@ using UnityEngine;
 
 public class SpriteScrollerLarge : MonoBehaviour
 {
-    // the image is 32x32 Unity units at 64 pixels-per-unit resolution scale (1, 1)
-    private const int HALF_WIDTH = 16;
-    private const int HALF_HEIGHT = 16;
+    // this is basically exactly the same as the small version, but 8x larger
+    // TODO: consolidate SpriteScroller and SpriteScrollerLarge
+    private const int HALF_WIDTH = 128;
+    private const int HALF_HEIGHT = 128;
 
     void Update()
     {
         var bottomLeft = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
         var topRight = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
 
-        var isOutOfBounds = bottomLeft.x < transform.position.x - HALF_WIDTH + 1 ||
-            bottomLeft.y < transform.position.y - HALF_HEIGHT + 1 ||
-            topRight.x > transform.position.x + HALF_WIDTH - 1 ||
-            topRight.y > transform.position.y + HALF_HEIGHT - 1;
+        var isOutOfBounds = bottomLeft.x < transform.position.x - HALF_WIDTH + 8 ||
+            bottomLeft.y < transform.position.y - HALF_HEIGHT + 8 ||
+            topRight.x > transform.position.x + HALF_WIDTH - 8 ||
+            topRight.y > transform.position.y + HALF_HEIGHT - 8;
 
         if (isOutOfBounds)
         {
