a2f280045b8c0bb2f88f5da136516fb1ca442b17
tabrown3 <t-a-brown@hotmail.com>
2023-05-22 16:48:57 -0500

    create LessThanGate

---
+++ Assets/Scripts/Scrapset/Gates/Conditionals/LessThanGate.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+
+public class LessThanGate : Gate, IExpression
+{
+    public override string Name => "Less Than";
+
+    public override string Description => "Compares inputs A and B and returns true if A is less than B";
+
+    public override string Category => "Conditionals";
+
+    public LessThanGate()
+    {
+        InputParameters.Add("A", ScrapsetTypes.Number);
+        InputParameters.Add("B", ScrapsetTypes.Number);
+        OutputParameters.Add("Out", ScrapsetTypes.Bool);
+    }
+
+    public Dictionary<string, ScrapsetValue> Evaluate(Dictionary<string, ScrapsetValue> inputs)
+    {
+        var a = (float)inputs["A"].Value;
+        var b = (float)inputs["B"].Value;
+        var result = a < b;
+        var outVal = new ScrapsetValue(ScrapsetTypes.Bool);
+        outVal.Value = result;
+        return new Dictionary<string, ScrapsetValue>() { { "Out", outVal } };
+    }
+}
