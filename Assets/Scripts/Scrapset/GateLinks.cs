77b7e2b9748f9dabbe0a9758afbc44d289a2dd80
tabrown3 <t-a-brown@hotmail.com>
2023-05-23 20:55:30 -0500

    test

---
+++ Assets/Scripts/Scrapset/GateLinks.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+public class GateLinks
+{
+    // linksByGateIdInputParam is a deep Dictionary storing all the I/O links by the calling gate's ID and input param name.
+    //  Outer key is IGate.Id, inner key is InputParameterName
+    public Dictionary<string, GateLink> Inputs = new Dictionary<string, GateLink>();
+    // linksByGateIdOutputParam is a deep Dictionary storing all the I/O links by the source (outputting) gate's ID and output param name.
+    //  Outer key is IGate.Id, inner key is InputParameterName, value is a list of all GateLinks for which this output is a data source
+    public Dictionary<string, List<GateLink>> Outputs = new Dictionary<string, List<GateLink>>();
+}
