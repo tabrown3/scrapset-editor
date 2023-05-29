1a2212017e17e27086826c8a7d9d48f7fdaa45ad
tabrown3 <t-a-brown@hotmail.com>
2023-05-29 12:39:06 -0500

    more folder shuffling

--- Assets/Scripts/ScrapsetEditor/ScrapsetEditorController.cs
+++ Assets/Scripts/ScrapsetEditor/ScrapsetEditorController.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EditorManager : MonoBehaviour
+public class ScrapsetEditorController : MonoBehaviour
 {
+    [SerializeField] SubroutineManager SubroutineManager;
+
     [SerializeField] GameObject EntrypointPrefab;
     [SerializeField] GameObject ExpressionPrefab;
     [SerializeField] GameObject SRInputPrefab;
@@ -13,13 +15,8 @@ public class EditorManager : MonoBehaviour
 
     Dictionary<string, GameObject> srGameObjects = new Dictionary<string, GameObject>();
 
-    SubroutineManager subroutineManager;
-
     void Start()
     {
-        if (subroutineManager == null)
-        {
-            subroutineManager = FindObjectOfType<SubroutineManager>();
-        }
+
     }
 }
