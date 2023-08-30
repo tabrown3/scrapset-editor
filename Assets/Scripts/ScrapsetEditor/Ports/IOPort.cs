96dd68edf471ba306b9fe71056a89918181ea96d
tabrown3 <t-a-brown@hotmail.com>
2023-08-30 15:29:26 -0500

    only attach on WorldCursor collision

--- Assets/Scripts/ScrapsetEditor/Ports/IOPort.cs
+++ Assets/Scripts/ScrapsetEditor/Ports/IOPort.cs
@@ -8,22 +8,25 @@ namespace Scrapset.Editor
         [SerializeField] RenderSide renderSide;
 
         GateIOContextMenu menu;
+        WorldCursor worldCursor;
 
         private void Start()
         {
             menu = FindObjectOfType<GateIOContextMenu>();
+            worldCursor = FindObjectOfType<WorldCursor>();
         }
 
         void OnTriggerEnter2D(Collider2D collision)
         {
-            if (menu == null) return;
+            if (menu == null || worldCursor == null) return;
+            if (collision.gameObject != worldCursor.gameObject) return; // only attach on WorldCursor collision
 
             menu.Attach(gameObject, renderSide);
         }
 
         void OnTriggerExit2D(Collider2D collision)
         {
-            if (menu == null) return;
+            if (menu == null || worldCursor == null) return;
 
             menu.Detach();
         }
